c1945604d53a9c2cf45e4ed53db0a34d182f0f46
Ari <sotoariadna730@gmail.com>
2024-03-24 23:25:34 -0600

    prueba

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/*dynamic buscador  ; 
+Console.WriteLine("Ingresa lo que buscas ");
+buscador=Console.ReadLine();
+
+
+// aparece en patalla lo que buscas 
+Console.WriteLine();
+Console.WriteLine("Sugerencias de tu busqueda:  "+ buscador);
+Console.ReadKey(); */
+
+int num1, num2;
+ Console.WriteLine("Ingresa el valor para numero 1 ");
+ num1=int.Parse(Console.ReadLine());
+ Console.WriteLine("Ingresa el valor para el numero dos ");
+ num2=int.Parse(Console.ReadLine());
+ Console.WriteLine(num1<num2);
+ Console.ReadKey();
